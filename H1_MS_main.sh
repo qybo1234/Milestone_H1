803a85f02ee8cbe167a4147a1e9bb0a6d23011bb
qybo1234 <yubo.qin@rutgers.edu>
2019-11-13 11:47:35 -0500

    Add while loop to ensure submit client job after server launched. Add job wait time measurement.

--- H1_MS_main.sh
+++ H1_MS_main.sh
@@ -3,15 +3,30 @@
 # Absolute path to the dataspaces root directory
 DIR=/home1/yq47/dataspaces
 
+#Get server job submition timestamp
+start_ts_server=$(date +%s)
+
 # Submit DataSpaces server job
 echo "Submit DataSpaces server ..."
 sbatch $DIR/tests/C/H1_MS_server.sh
 
-echo "Waiting to submit client jobs in 5 sec ..."
-sleep 5
+echo "Waiting to DataSpaces server job to launch ..."
+
+while [ ! -f dataspaces.conf ]; do
+    sleep 1s
+done
+
+end_ts_server=$(date +%s)
+echo "DataSpaces server launched after waiting $((end_ts_server - start_ts_server)) sec"
 
 # Submit DataSpaces client job
 echo "Submit DataSpaces clients ..."
+start_ts_client=$(date +%s)
 sbatch $DIR/tests/C/H1_MS_client.sh
 
-echo "Please wait a moment and check the server.log, writer.log, and reader.log"
+while [ ! -f writer.log ]; do
+    sleep 1s
+done
+end_ts_client=$(date +%s)
+echo "DataSpaces clients launched after waiting $((end_ts_client - start_ts_client)) sec"
+echo "Please wait a moment and check the server.log, writer.log, and reader.log!"
